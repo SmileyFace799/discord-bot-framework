66742febfd5eba43b5b8998f22665b7bb25b6ed1
SmileyFace799 <Henrikgran85@gmail.com>
2024-07-13 14:35:21 +0200

    v1.4.6: Temporarily removed responsibility of managing bot tokens

--- src/main/java/no/smileyface/discordbotframework/DiscordBot.java
+++ src/main/java/no/smileyface/discordbotframework/DiscordBot.java
@@ -1,6 +1,5 @@
 package no.smileyface.discordbotframework;
 
-import java.nio.file.NoSuchFileException;
 import java.util.Arrays;
 import java.util.Collection;
 import net.dv8tion.jda.api.JDA;
@@ -19,19 +18,19 @@ public final class DiscordBot {
 
 	/**
 	 * Creates the discord bot. To create a bot with a custom
-	 * {@link ActionManager}, use {@link #create(ActionManager, GatewayIntent...)} instead.
+	 * {@link ActionManager}, use {@link #create(String, ActionManager, GatewayIntent...)} instead.
 	 *
 	 * @param actions Collection of actions to create the bot with.
 	 * @return The created bot JDA instance
-	 * @throws NoSuchFileException  If the token file for the active bot is not found
 	 * @throws InterruptedException If the bot is interrupted while starting
-	 * @see #create(ActionManager, GatewayIntent...)
+	 * @see #create(String, ActionManager, GatewayIntent...)
 	 */
 	public static JDA create(
+			String botToken,
 			Collection<? extends BotAction<? extends BotAction.ArgKey>> actions,
 			GatewayIntent... intents
-	) throws NoSuchFileException, InterruptedException {
-		return create(new ActionManager(actions), intents);
+	) throws InterruptedException {
+		return create(botToken, new ActionManager(actions), intents);
 	}
 
 	/**
@@ -39,16 +38,16 @@ public final class DiscordBot {
 	 *
 	 * @param actionManager The {@link ActionManager} for executing actions
 	 * @return The created bot JDA instance
-	 * @throws NoSuchFileException  If the token file for the active bot is not found
 	 * @throws InterruptedException If the bot is interrupted while starting
-	 * @see #create(Collection, GatewayIntent...)
+	 * @see #create(String, Collection, GatewayIntent...)
 	 */
 	public static JDA create(
+			String botToken,
 			ActionManager actionManager,
 			GatewayIntent... intents
-	) throws NoSuchFileException, InterruptedException {
+	) throws InterruptedException {
 		JDABuilder builder = JDABuilder
-				.createDefault(TokenManager.getActiveBot(), Arrays.asList(intents))
+				.createDefault(botToken, Arrays.asList(intents))
 				.disableCache(CacheFlag.EMOJI, CacheFlag.STICKER, CacheFlag.SCHEDULED_EVENTS)
 				.addEventListeners(actionManager);
 		if (!Arrays.asList(intents).contains(GatewayIntent.GUILD_VOICE_STATES)) {
